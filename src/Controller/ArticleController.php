7e569316d1f8e9c7f12f186eaf088b40fd52489d
Eriatea <nastya.ovchinnikova89@gmail.com>
2023-10-31 22:11:56 +0300

    add fixtures

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,34 +2,17 @@
 
 namespace App\Controller;
 
-use App\Entity\Article;
-use App\Service\ArticlesCreatorProvider;
-use Doctrine\ORM\EntityManagerInterface;
-use App\Service\ArticleContentProvider;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class ArticleController extends AbstractController
 {
-    private $articleCreator;
-
     /**
-     * @param ArticlesCreatorProvider $articleCreator
+     * @Route("/admin/articles/create", name="admin_articles_create")
      */
-    public function __construct(ArticlesCreatorProvider $articleCreator)
+    public function create(): Response
     {
-        $this->articleCreator = $articleCreator;
+        return new Response('Здесь будет страница создания статьи');
     }
 
-    /**
-     * @Route("/admin/articles/create", name="app_admin_articles_create")
-     */
-    public function create(ArticleContentProvider $articleContentProvider): Response
-    {
-        $article = $this->articleCreator->create($articleContentProvider);
-
-        return new Response(sprintf('Создана статья id: %d',
-            $article->getId()));
-    }
 }
