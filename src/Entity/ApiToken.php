132821cfc1b8a43869da4cd6edcd14e4605a21d0
Eriatea <nastya.ovchinnikova89@gmail.com>
2023-11-03 21:05:39 +0300

    add ApiToken entity and ApiTokenAuthenticator for token auth

---
+++ src/Entity/ApiToken.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ApiTokenRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ApiTokenRepository::class)
+ */
+class ApiToken
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private string $token;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private \DateTime $expiresAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="apiTokens")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private object $user;
+
+    /**
+     * @param User $user
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+        $this->token = sha1(uniqid('token'));
+        $this->expiresAt = new \DateTime('+1 day');
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getToken(): ?string
+    {
+        return $this->token;
+    }
+
+    /**
+     * @return \DateTimeInterface|null
+     */
+    public function getExpiresAt(): ?\DateTimeInterface
+    {
+        return $this->expiresAt;
+    }
+
+    /**
+     * @return User|null
+     */
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isExpired(): bool
+    {
+        return $this->getExpiresAt() <= new \DateTime();
+    }
+}
